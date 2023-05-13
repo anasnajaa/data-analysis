4d5536763effa4529d2e8b19fad2a6c6759ff174
Anas Najaa <spidernet12@gmail.com>
2023-05-13 12:15:56 +0300

    a2_q2 done

---
+++ answers/a2_q2.js
@@ -0,0 +1,39 @@
+const {normalizedCountries} = require('../util/normalize');
+
+exports.a2_q2 = async () => {
+    const countries = await normalizedCountries();
+
+    const yearsMedian = [];
+    
+    countries.forEach(country => {
+        country.years.forEach(year => {
+            if(!year.year) return;
+            const yearFound = yearsMedian.find(x => x.year === year.year);
+            if(yearFound){
+                yearFound.healthyLifeExpectencyAtBirthTotal += year.healthyLifeExpectencyAtBirth.both;
+                yearFound.lifeExpecAtBirthTotal += year.lifeExpecAtBirth.both;
+                yearFound.count += 1;
+            } else {
+                let temp = {
+                    year: year.year,
+                    healthyLifeExpectencyAtBirthTotal: 0,
+                    lifeExpecAtBirthTotal: 0,
+                    count: 0
+                };
+                temp.healthyLifeExpectencyAtBirthTotal += year.healthyLifeExpectencyAtBirth.both;
+                temp.lifeExpecAtBirthTotal += year.lifeExpecAtBirth.both;
+                temp.count += 1;
+                yearsMedian.push(temp);
+            }
+        })
+    });
+
+    yearsMedian.forEach(year => {
+        year.lifeExpecAtBirthMedian = year.lifeExpecAtBirthTotal / year.count;
+        year.healthyLifeExpectencyAtBirthMedian = year.healthyLifeExpectencyAtBirthTotal / year.count;
+    });
+
+    console.log(yearsMedian)
+
+    return yearsMedian;
+}
--- routes/pages.js
+++ routes/pages.js
@@ -5,6 +5,7 @@ const {a1_q2} = require('../answers/a1_q2');
 const {a1_q3} = require('../answers/a1_q3');
 const {a1_q4} = require('../answers/a1_q4');
 const {a1_q5} = require('../answers/a1_q5');
+
 const {b1_q6} = require('../answers/b1_q6');
 const {b1_q7} = require('../answers/b1_q7');
 const {b1_q8} = require('../answers/b1_q8');
@@ -12,6 +13,8 @@ const {b1_q9} = require('../answers/b1_q9');
 const {b1_q10} = require('../answers/b1_q10');
 
 const {a2_q1} = require('../answers/a2_q1');
+const {a2_q2} = require('../answers/a2_q2');
+const {a2_q3} = require('../answers/a2_q3');
 
 
 const router = express.Router();
@@ -71,6 +74,16 @@ router.get("/b1_q10", async (req, res, next) => {
 router.get("/a2_q1", async (req, res, next) => {
     const data = await a2_q1();
     res.render('pages/a2_q1', { data });
+});
+
+router.get("/a2_q2", async (req, res, next) => {
+    const data = await a2_q2();
+    res.render('pages/a2_q2', { data });
+});
+
+router.get("/a2_q3", async (req, res, next) => {
+    const data = await a2_q3();
+    res.render('pages/a2_q3', { data });
 });
 
 module.exports = router;
