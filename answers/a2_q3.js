88f6debef0112cc449e7a54800cb94b717414f96
Anas Najaa <spidernet12@gmail.com>
2023-05-13 12:16:05 +0300

    started a2_q3

---
+++ answers/a2_q3.js
@@ -0,0 +1,52 @@
+const {normalizedCountries} = require('../util/normalize');
+
+exports.a2_q3 = async () => {
+    const countries = await normalizedCountries();
+    let years = [];
+
+    countries.forEach(country => {
+        country.years.forEach(year => {
+            if(!year.year) return;
+            if(year >= 2000 && year <= 2019){
+                const yearFound = years.find(x => x.year === year.year);
+                if(yearFound){
+
+                    year.ranges.forEach(range => {
+                        if(year.healthyLifeExpectencyAtBirth.male >= range.low &&
+                            year.healthyLifeExpectencyAtBirth.male <= range.high ){
+                            range.data.push(year.healthyLifeExpectencyAtBirth.male);
+                        }
+                    })
+
+                } else {
+                    let temp = { year: year.year, ranges: [] }
+                    let counter = 30;
+                
+                    while (counter <= 90){
+                        ranges.push({
+                            low: counter,
+                            high: counter+5,
+                            label: `${counter} - ${counter+5}`,
+                            data: []
+                        });
+                        counter+=5;
+                    }
+
+                    
+                    temp.ranges.forEach(range => {
+                        if(year.healthyLifeExpectencyAtBirth.male >= range.low &&
+                            year.healthyLifeExpectencyAtBirth.male <= range.high ){
+                            range.data.push(year.healthyLifeExpectencyAtBirth.male);
+                        }
+                    })
+                    
+                    years.push(temp);
+                }
+            }
+        });
+    });
+
+    console.log(years)
+
+    return years;
+}
